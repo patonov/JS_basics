31e4ff44619b6984d4f0a00143a8735fa5d5514d
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-08-01 19:24:31 +0300

    Add files via upload

---
+++ ChineseGreeting/ChineseGreeting.js
@@ -0,0 +1,10 @@
+var salve = "Zhao cai jin bao "
+
+function chineseGreetingFunction(){
+    
+    var whom = "Nikolay Patonov!";
+    
+    console.log(salve + whom);
+}
+
+chineseGreetingFunction();
