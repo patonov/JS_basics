63160fccef54ba08c653ee498da226b193caccaa
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-08-04 22:43:43 +0300

    Add files via upload

---
+++ NameAndAgeArrays/NameAgeArr.js
@@ -0,0 +1,6 @@
+let nameArr = ['Pesho', 'Gosho', 'Misho'];
+let ageArr = [22, 33, 44];
+
+console.log(nameArr[0] + ' ' + ageArr[0]);
+console.log(nameArr[1] + ' ' + ageArr[1]);
+console.log(nameArr[2] + ' ' + ageArr[2]);
