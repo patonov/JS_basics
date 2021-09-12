70d9e42894ee4ce4e5458fa287ee54a6b757589a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-09-12 23:11:08 +0300

    Add files via upload

---
+++ JSConditionalGreeting/let time = 9;.js
@@ -0,0 +1,16 @@
+let time = 9;
+
+if (time <= 12) 
+{
+  greeting = "Good morning";
+} 
+else if (time <= 17) 
+{
+  greeting = "Good afternoon";
+} 
+else 
+{
+  greeting = "Good evening";
+}
+
+console.log(greeting);
