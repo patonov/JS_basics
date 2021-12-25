18d01639f6e8f2b017c1f35daee8ab060480380d
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-12-25 23:15:20 +0200

    Add files via upload

---
+++ FirstTask/MyFirstTask.js
@@ -0,0 +1,26 @@
+function hello() {
+    console.log("Hello, weeeey");
+}
+
+hello();
+
+function printNums() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+}
+
+printNums();
+
+function printStars() {
+    console.log('*');
+    console.log('**');
+    console.log('***');
+    console.log('****');
+    console.log('*****');
+}
+
+printStars();
+
