98b677eafd0953a46142c0313db6ee5330235061
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-12-26 13:24:47 +0200

    Add files via upload

---
+++ JSvariables/varsJS.js
@@ -0,0 +1,31 @@
+function readName(input) {
+let str = input;
+console.log(str);
+}
+
+readName("Nikolay");
+
+function readNumber(input) {
+let num = Number(input);
+num = num + 3;
+console.log(num);
+}
+
+readNumber("2");
+
+function sqArea(input) {
+let side = Number(input);
+let area = side * side;
+console.log(area);
+}
+
+sqArea(3);
+
+function subtractingOfDoubles(num1, num2) {
+let first = Number(num1);
+let second = Number(num2);
+let result = first / second;
+console.log(result);
+} 
+
+subtractingOfDoubles(20.0, 3.0);
